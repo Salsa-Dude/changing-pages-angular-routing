e55bda67f01fa04bc2a3e72e64bc87ee972b816f
Joseph Arias <arias.joseph.a@gmail.com>
2021-10-10 19:05:56 -0400

    Setting up and Loading Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent }
+]
 
 @NgModule({
   declarations: [
@@ -25,8 +32,12 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+   // The forRoot static method is the method that configures the root routing module for your app.
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
